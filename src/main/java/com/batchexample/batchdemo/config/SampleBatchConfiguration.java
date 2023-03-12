4d651634a2645b1a75a3e17172c8db3204af861e
Dipesh Pawar <Dipeshgit7@gmail.com>
2023-03-13 00:09:33 +0530

    added code to get list from db

--- src/main/java/com/batchexample/batchdemo/config/SampleBatchConfiguration.java
+++ src/main/java/com/batchexample/batchdemo/config/SampleBatchConfiguration.java
@@ -1,9 +1,15 @@
 package com.batchexample.batchdemo.config;
 
+import com.batchexample.batchdemo.exception.CustomBatchException;
+import com.batchexample.batchdemo.repository.MerchantRepository;
 import com.batchexample.batchdemo.tasks.SampleBatchTasklet;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,30 +20,46 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableBatchProcessing
 public class SampleBatchConfiguration {
+  @Autowired JobBuilderFactory jobBuilderFactory;
+  @Autowired StepBuilderFactory stepBuilderFactory;
+  @Autowired MerchantRepository merchantRepository;
 
-    @Autowired JobBuilderFactory jobBuilderFactory;
-
-    @Autowired StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job processJob(){
-        return jobBuilderFactory.get("SAMPLEJOB").incrementer(new RunIdIncrementer()).start(processStep1()).next(processStep2()).build();
+  @Bean
+  public Job processJob() throws CustomBatchException {
+    try {
+      return jobBuilderFactory
+          .get("SAMPLEJOB")
+          .incrementer(new RunIdIncrementer())
+          .start(processStep1())
+          .next(processStep2())
+          .build();
+    } catch (CustomBatchException e) {
+      throw new CustomBatchException(e.getMessage());
     }
+  }
 
-    @Bean
-    public Step processStep1(){
-        return stepBuilderFactory.get("SAMPLESTEP1").tasklet(tasklet()).build();
-    }
-    @Bean
-    public Step processStep2(){
-    return stepBuilderFactory.get("SAMPLESTEP2").tasklet(new SampleBatchTasklet()).build();
-    }
+  @Bean
+  public Step processStep1() {
+    return stepBuilderFactory.get("SAMPLESTEP1").tasklet(tasklet()).build();
+  }
 
-    @Bean
-    public Tasklet tasklet() {
+  @Bean
+  public Step processStep2() throws CustomBatchException {
+    try {
+      return stepBuilderFactory
+          .get("SAMPLESTEP2")
+          .tasklet(new SampleBatchTasklet(merchantRepository))
+          .build();
+    } catch (CustomBatchException e) {
+      throw new CustomBatchException(e.getMessage());
+    }
+  }
+
+  @Bean
+  public Tasklet tasklet() {
     return (contribution, chunkContext) -> {
-    log.info("processing first step");
+      log.info("processing first step");
       return RepeatStatus.FINISHED;
     };
-    }
+  }
 }
--- src/main/java/com/batchexample/batchdemo/tasks/SampleBatchTasklet.java
+++ src/main/java/com/batchexample/batchdemo/tasks/SampleBatchTasklet.java
@@ -1,18 +1,31 @@
 package com.batchexample.batchdemo.tasks;
 
-import lombok.extern.log4j.Log4j2;
+import com.batchexample.batchdemo.dto.MerchantDto;import com.batchexample.batchdemo.exception.CustomBatchException;import com.batchexample.batchdemo.repository.MerchantRepository;import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;import java.util.List;
 
 @Log4j2
 public class SampleBatchTasklet implements Tasklet {
 
+ private MerchantRepository merchantRepository;
+
+ public SampleBatchTasklet(MerchantRepository merchantRepository){
+   this.merchantRepository = merchantRepository;
+ }
   @Override
   public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
       throws Exception {
+    try{
     log.info("calling in smaple batch tasklet");
+    List<MerchantDto> merchantDtoList = merchantRepository.findAllMerchants();
+    log.info(merchantDtoList.size());
+    }catch (Exception e){
+      log.info("unable to process the batch error : {}",e.getMessage());
+      throw new CustomBatchException(e.getMessage());
+    }
     return RepeatStatus.FINISHED;
   }
 }
