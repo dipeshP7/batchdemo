bccba3f26d662ce771ac250ba9c1655281dd8a46
Dipesh Pawar <Dipeshgit7@gmail.com>
2023-03-18 00:18:58 +0530

    added another value

--- src/main/java/com/batchexample/batchdemo/utils/ConstantNumber.java
+++ src/main/java/com/batchexample/batchdemo/utils/ConstantNumber.java
@@ -4,4 +4,6 @@ public class ConstantNumber {
   public static final int ZERO = 0;
   public static final int ONE = 1;
   public static final int THREE = 3;
+  public static final int SEVEN = 7;
+
 }
