af7217b4614218dc9fdc1f838dc0c3bf86e62e70
Dipesh Pawar <Dipeshgit7@gmail.com>
2023-03-17 22:50:55 +0530

    ﻿rebase the conflict

--- src/main/java/com/batchexample/batchdemo/utils/ConstantNumber.java
+++ src/main/java/com/batchexample/batchdemo/utils/ConstantNumber.java
@@ -4,6 +4,7 @@ public class ConstantNumber {
   public static final int ZERO = 0;
   public static final int ONE = 1;
   public static final int THREE = 3;
+  public static final int FIVE = 5;
   public static final int SEVEN = 7;
 
 }
