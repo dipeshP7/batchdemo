6aed182d2b876afb7692192e92b7487b67e7588f
Dipesh Pawar <Dipeshgit7@gmail.com>
2023-03-18 18:26:56 +0530

    new value added

--- src/main/java/com/batchexample/batchdemo/utils/NameUtils.java
+++ src/main/java/com/batchexample/batchdemo/utils/NameUtils.java
@@ -2,4 +2,5 @@ package com.batchexample.batchdemo.utils;
 
 public class NameUtils {
     public static final String REPORT_NAME = "BATCH REPORT";
+    public static final String REPORT_TITLE = "BATCH TITLE";
 }
