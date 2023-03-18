3477cb2938937ee5044f7b7d3f603d4593be2b0e
Dipesh Pawar <Dipeshgit7@gmail.com>
2023-03-18 18:20:33 +0530

    remove value

--- src/main/java/com/batchexample/batchdemo/utils/NameUtils.java
+++ src/main/java/com/batchexample/batchdemo/utils/NameUtils.java
@@ -2,5 +2,4 @@ package com.batchexample.batchdemo.utils;
 
 public class NameUtils {
     public static final String REPORT_NAME = "BATCH REPORT";
-    public static final String REPORT_TITLE = "REPORT TITLE";
 }
